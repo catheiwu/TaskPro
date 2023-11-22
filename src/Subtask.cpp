51f2235f188cb294d0a5e02c98341b2b71fd0bd5
faris-mohammady <146904259+faris-mohammady@users.noreply.github.com>
2023-11-21 21:33:54 -0800

    Update Subtask.cpp

--- src/Subtask.cpp
+++ src/Subtask.cpp
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-#include "../include/Subask.h"
 
-Subtask::Subtask()
-=======
 #include "../include/SubTask.h"
 
 SubTask::SubTask()
->>>>>>> faris/priority
 {
 
 }
-
-<<<<<<< HEAD
 Subtask::~Subtask()
 {
     
 }
-=======
 SubTask::~SubTask()
 {
     
 }
->>>>>>> faris/priority
