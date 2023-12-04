e6038cafd969d7e8061e820a1e73f1f4c49b95cc
Banff Jiang <bjian018@xe-01.cs.ucr.edu>
2023-12-04 01:33:55 -0800

    Main Task functions implemented

--- src/MainTask.cpp
+++ src/MainTask.cpp
@@ -20,18 +20,28 @@ MainTask::~MainTask(){
 
 bool MainTask::isRecurring() {
 
+    if (recurringEventTime == 0) {
 
-return true;
+        return false;
+    }
+    
+    return true;
+
 }
+
 
 bool MainTask:: isDdlPassed() {
 
-return true;
+    return ddlPassed;
+
 }
 
 int MainTask::addSubtask(SubTask* newSubtask) {
 
-return 0;
+  // Task *taskToAdd = new MainTask(taskName);
+    allSubtasks.push_back(newSubtask);
+
+    return 0;
 }
 
 time_t MainTask::getDdl() {
@@ -54,7 +64,7 @@ return ;
 }
 
 
-void MainTask::editRecurringEventTime(int newRecurringEventTime) {
+void MainTask::editRecurringEventTime(uint newRecurringEventTime) {
 
 
 }
@@ -68,11 +78,36 @@ return q->allSubtasks;
 
 void MainTask::editDdlPassed(bool passed) {
 
+    ddlPassed = passed;
+}
 
+int MainTask::updateDdlPassed() {
+
+    time_t timep;
+
+    time(&timep);
+    if (timep > getDdl()) { // which means deadline has already passed
+
+    editDdlPassed(true);
+
+    }
+    else {
+        editDdlPassed(false);
+    }
+
+    return 0; 
 }
 
 int MainTask::deleteSubtask(int index) {
 
-return 0;
+    if (taskIndex < 1 || taskIndex > allSubtasks.size())
+    {
+        // cout << "Invalid task index. Please enter a valid index." << endl;
+        return -1;
+    }
+    delete allSubtasks[taskIndex - 1];
+    allSubtasks.erase(allSubtasks.begin() + taskIndex - 1);
+    // cout << "Task deleted successfully!" << endl;
 }
+
 
