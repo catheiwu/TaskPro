36b305b26caf92a30126deaa1e7c3c5307bb7fb4
Cathy Wu <cwu247@xe-01.cs.ucr.edu>
2023-12-04 17:38:10 -0800

    update main.cpp and helperFunctions (both unfinished)

---
+++ src/helperFunctions.cpp
@@ -0,0 +1,72 @@
+#include "TaskList.h"
+#include "Task.h"
+#include "Subtask.h"
+#include "MainTask.h"
+#include "helperFunctions.h" 
+
+using namespace std;
+
+void displayTasks(TaskList* taskList){
+    taskList->getAllSubtasks();
+}
+
+void taskToAdd(TaskList* taskList){
+
+    int taskNumber;
+    cout << "Enter the task number: ";
+    cin.ignore();  // To clear the buffer before getline
+    getline(cin, newTask);
+    taskList->addTask();
+    cout << "Task added successfully!\n";
+}
+
+void taskToDelete(TaskList* taskList){
+
+    int taskNumber;
+    cout << "Enter the task number: ";
+    cin.ignore();  // To clear the buffer before getline
+    getline(cin, taskNumber);
+    taskList->deleteTask();
+    cout << "Task deleted successfully!\n";
+
+}
+
+void taskToEdit(TaskList* taskList){
+
+    int editChoice;
+
+    cout << "Enter 1 - Edit task name." << endl;
+    cout << "Enter 2 - Edit task description." << endl;
+    cout << "Enter 3 - Edit task priority." << endl;
+    cout << "Enter 4 - Edit task deadline." << endl;
+    cin >> sortChoice;
+
+    if(userChoice == 1){
+        taskList->editName();
+    } else if (userChoice == 2){
+        taskList->editDescription();
+    } else if (userChoice == 3){
+        taskList->editPriority;
+    } else if (userChoice == 4){
+        taskList->editDdl();
+    } else {
+        cout << "Invalid choice. Please enter a number between 1 and 4.\n";
+    }
+}
+
+
+void sortTasks(TaskList* taskList){
+
+    int sortChoice;
+
+    cout << "Enter 1 - Sort by priority. Enter 2 - Sort by deadline."
+    cin >> sortChoice;
+
+    if (userChoice == 1){
+        taskList->sort(1);
+    } else if (userChoice == 2){
+        taskList->sort(2);
+    } else {
+        cout << "Invalid choice. Please enter a number between 1 and 2.\n";
+    }
+}
